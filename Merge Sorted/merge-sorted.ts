60ce430d64a0335c7ec1d05f97d1a7036686b756
Ryan Griggs <griggsspace@gmail.com>
2023-06-22 23:17:24 -0700

    Merge Sorted complete and submitted

--- Merge Sorted/merge-sorted.ts
+++ Merge Sorted/merge-sorted.ts
@@ -26,47 +26,16 @@
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
-function mergeSorted(nums1: number[], nums2: number[], m: number, n: number): void {
-  //run while j < (m + n)
-  let j: number = 0;
-  let i: number = 0;
-  //run while j is less than m+n
-  while( j < ( m + n ) ){
-      //if nums2[i] <= nums1[i] splice it into nums1[i-1] and increment i
-      if(nums2[i] <= nums1[j] || nums2[i] && !nums1[j]){
-        nums1.splice(j, 0, nums2[i]);
-        i++;
-      }
-      j++;
-  }
+function mergeSorted(nums1: number[], m: number, nums2: number[], n: number): void {
+ 
+  nums2.forEach((el, i, arr) => {
+    nums1[m+i] = el
+  });
+  nums1.sort((a,b) => a-b);
+  console.log(nums1)
 }
 
-function mergeSortedPure(nums1: number[], nums2: number[], m: number, n: number): number[] {
-
-  //solve problem here
-  let i: number = 0, j:number = 0;
-  const result: number[] = [];
-
-  //run while i<num1.length or j < num2.length
-  while( i < nums1.length || j < nums2.length ) {
-
-    //push nums1[i] if nums1[i] < nums2[j] then increment i
-    if(nums1[i] <= nums2[j] || nums1[i] && !nums2[j]) {
-      result.push(nums1[i]);
-      i++;
-    }
-    //push nums2[j] if nums2[j] < nums1[i] then increment j
-    else if(nums2[j] <= nums1[i] || nums2[j] && !nums1[i]) {
-      result.push(nums2[j]);
-      j++;
-    }
-  } 
-  //return result array
-  return result;
-}
-
-const result = mergeSorted([5,6,7], [1,6,9], 3, 3);
-console.log(result)
+const result = mergeSorted([1,2,3,0,0,0], 3, [2,5,6], 3);
 
 
 module.exports = mergeSorted;
