c38d202857fe68856d691724ba7cfae8d9ce63d1
Ryan Griggs <griggsspace@gmail.com>
2023-06-23 21:29:29 -0700

    remove elements complete - no tests

--- __tests__/merge-sorted-test.ts
+++ __tests__/merge-sorted-test.ts
@@ -1,18 +1,7 @@
-const testMergeSorted = require('../Merge Sorted/merge-sorted');
+const testMergeSorted = require('../merge-sorted');
 
-test('** check return value is of type Array **', () => {
-  const testArr: number[] = testMergeSorted([17,11,35], [4,15,9], 3, 3);
-  expect(Array.isArray(testArr)).toBe(true);
-})
-
-test('** check returned array length equals (m + n) **', () => {
-  const testArr = testMergeSorted([1,2,3], [4,5,6], 3, 3);
-  expect(testArr.length).toBe(6);
-});
-
-test('** check returned array is sorted with ascending pattern **', () => {
-  const testArr: number[] = testMergeSorted([1,11,35], [4,5,9], 3, 3);
-  console.log('testArr:', testArr);
+test('** check nums1 array is sorted with ascending pattern **', () => {
+  const testArr: number[] = testMergeSorted([1,11,35], 3, [4,5,9], 3);
   let isSorted: boolean = true;
   for( let i = 1; i < testArr.length; i++ ) {
     if(testArr[i] < testArr[i-1]) {
